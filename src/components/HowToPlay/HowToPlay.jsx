1217293b652894a3e092d8bc80adfaf3bda070b5
Huu Phuoc <tpgnor@gmail.com>
2026-01-28 22:23:19 +0700

    feat: How to play

--- src/components/HowToPlay/HowToPlay.jsx
+++ src/components/HowToPlay/HowToPlay.jsx
@@ -1,5 +1,20 @@
+import "./HowToPlay.css";
+
 const HowToPlay = () => {
-    return <>How to play</>;
+    return (
+        <div className="how-to-play-troll">
+            <p className="troll-text">
+                🎮 JUST PLAY THE GAME <br />
+                WHILE <span className="duck">SERIOUS DUCK</span> <br />
+                IS IN YOUR DISCORD ROOM 🦆
+            </p>
+
+            <p className="sub-text">
+                (He will quack to guide you. He is serious. Do not disappoint
+                him. No question. Just play the game.)
+            </p>
+        </div>
+    );
 };
 
 export default HowToPlay;
